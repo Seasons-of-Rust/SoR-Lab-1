9e3fd36828b3aff36f5eda1e724d83080bfab601
Forest Anderson <forestkzanderson@gmail.com>
2022-04-25 08:32:48 -0400

    Added line to fix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
-        assert_eq!(2 + 2, 5);
+        assert_eq!(1 + 1, 3); // Fix this line! Both sides should be equal.
     }
 }
